2605e50740d6e5f005cbad3566c7ca58991838e5
Ilyas2805 <58884526+Ilyas2805@users.noreply.github.com>
2021-05-17 08:28:28 +0500

    Add files via upload

---
+++ signupCheck.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	require_once 'db.php';
+
+	$full_name = $_POST['full_name'];
+	$login = $_POST['login'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$password_confirm = $_POST['password_confirm']; 
+
+	if ($password === $password_confirm) {
+		$password = md5($password);
+		
+		mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `username`, `email`, `password`) VALUES (NULL, '$full_name', '$login', '$email', '$password')");
+
+		$_SESSION['message'] = 'Регистрация прошла успешно'; 
+		header('Location: ../CodingLab/login.php');
+	} else{
+		$_SESSION['message'] = 'Пароли не совпадают'; 
+		header('Location: ../CodingLab/signup.php');
+	}
+?>
