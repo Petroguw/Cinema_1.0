8e085ab50b8406a89b837083c8eb5c2a806244aa
renata242 <59351081+renata242@users.noreply.github.com>
2021-05-23 14:32:09 +0600

    Add files via upload

--- profile.php
+++ profile.php
@@ -1,10 +1,11 @@
-<?php 
-    session_start();
-    if(!$_SESSION['user']){
-            header('Location: ../CodingLab/login.php');
-    }
-?>
-
+<?php include('header.php');
+if(!isset($_SESSION['user']))
+{
+	header('location:login.php');
+}
+	$qry2=mysqli_query($con,"select * from tbl_movie where movie_id='".$_SESSION['movie']."'");
+	$movie=mysqli_fetch_array($qry2);
+	?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,41 +14,110 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cinema</title>
-    <link rel="stylesheet" href="signup.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
     <style>
     </style>
 </head>
 
-<body>
-    <?php
-        require 'header.php'
-    ?>
-	
-<div class="block">
-<div class="wrapper">
-    <div class="title">Profile</div>
-        <form>
-            <div>
-                <h2 style="color: #37003c;"><?= $_SESSION['user']['full_name'] ?></h2>
-            </div>
-            <div>
-                <a href="#"><?= $_SESSION['user']['email'] ?></a>
-            </div>
-            <div>
-                <a href="logout.php" style="color: #b7625c;">Logout</a>
-            </div>
-        </form>
+<div class="content">
+	<div class="wrap">
+		<div class="content-top">
+				<div class="section group">
+					<div class="about span_1_of_2">	
+						<h3>BOOKINGS</h3>
+						<?php include('msgbox.php');?>
+						<?php
+				$bk=mysqli_query($con,"select * from tbl_bookings where user_id='".$_SESSION['user']."'");
+				if(mysqli_num_rows($bk))
+				{
+					?>
+					<table class="table table-bordered">
+						<thead>
+						<th>Booking Id</th>
+						<th>Movie</th>
+						<th>Theatre</th>
+						<th>Screen</th>
+						<th>Show</th>
+						<th>Seats</th>
+						<th>Amount</th>
+						<th></th>
+						</thead>
+						<tbody>
+						<?php
+						while($bkg=mysqli_fetch_array($bk))
+						{
+							$m=mysqli_query($con,"select * from tbl_movie where movie_id=(select movie_id from tbl_shows where s_id='".$bkg['show_id']."')");
+							$mov=mysqli_fetch_array($m);
+							$s=mysqli_query($con,"select * from tbl_screens where screen_id='".$bkg['screen_id']."'");
+							$srn=mysqli_fetch_array($s);
+							$tt=mysqli_query($con,"select * from tbl_theatre where id='".$bkg['t_id']."'");
+							$thr=mysqli_fetch_array($tt);
+							$st=mysqli_query($con,"select * from tbl_show_time where st_id=(select st_id from tbl_shows where s_id='".$bkg['show_id']."')");
+							$stm=mysqli_fetch_array($st);
+							?>
+							<tr>
+								<td>
+									<?php echo $bkg['ticket_id'];?>
+								</td>
+								<td>
+									<?php echo $mov['movie_name'];?>
+								</td>
+								<td>
+									<?php echo $thr['name'];?>
+								</td>
+								<td>
+									<?php echo $srn['screen_name'];?>
+								</td>
+								<td>
+									<?php echo $stm['name'];?>
+								</td>
+								<td>
+									<?php echo $bkg['no_seats'];?>
+								</td>
+								<td>
+									Rs <?php echo $bkg['amount'];?>
+								</td>
+								<td>
+									<?php  if($bkg['ticket_date']<date('Y-m-d'))
+									{
+										?>
+										<i class="glyphicon glyphicon-ok"></i>
+										<?php
+									}
+									else
+									{?>
+									<a href="cancel.php?id=<?php echo $bkg['book_id'];?>">Cancel</a>
+									<?php
+									}
+									?>
+								</td>
+							</tr>
+							<?php
+						}
+						?></tbody>
+					</table>
+					<?php
+				}
+				else
+				{
+					?>
+					<h3>No Previous Bookings</h3>
+					<?php
+				}
+				?>
+					</div>							
+			</div>
+				<div class="clear"></div>		
+			</div>
+	</div>
 </div>
-</div>
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include('footer.php');?>
+<script type="text/javascript">
+	$('#seats').change(function(){
+		var charge=<?php echo $screen['charge'];?>;
+		amount=charge*$(this).val();
+		$('#amount').html("Rs "+amount);
+		$('#hm').val(amount);
+	});
+</script>
