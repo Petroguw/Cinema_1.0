eb96553eb47dbf9a6745509520bba35d5327359d
renata242 <59351081+renata242@users.noreply.github.com>
2021-05-23 14:30:11 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+  $host = "localhost";
+    $user = "root";                     //Your Cloud 9 username
+    $pass = "";                                  //Remember, there is NO password by default!
+    $db = "db_movie";                                  //Your database name you want to connect to
+     $con = mysqli_connect($host, $user, $pass, $db)or die(mysql_error());
+
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+}
+
+?>
