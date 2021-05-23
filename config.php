25ecea5b13a569d4940737d7e7dfa14e29ca16a3
renata242 <59351081+renata242@users.noreply.github.com>
2021-05-23 14:30:56 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
   $host = "localhost";
-    $user = "root";                     //Your Cloud 9 username
-    $pass = "";                                  //Remember, there is NO password by default!
-    $db = "db_movie";                                  //Your database name you want to connect to
+    $user = "root";                     
+    $pass = "";                                  
+    $db = "db_movie";                                  
      $con = mysqli_connect($host, $user, $pass, $db)or die(mysql_error());
 
 if ($con->connect_error) {
