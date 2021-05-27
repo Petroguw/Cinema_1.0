c957db55ac860a3a2286f9f6e0c85576c1a0670f
renata242 <59351081+renata242@users.noreply.github.com>
2021-05-27 13:30:59 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cinema</title>
+    <link rel="stylesheet" type="text/css" href="Main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
     <style>
@@ -15,54 +16,39 @@
   <?php
   require 'header.php'
   ?>
-<div class="content" style="margin:20px">
+<div class="content" >
 	<div class="wrap">
-		<div class="content-top">
-<div class="listview_1_of_3 images_1_of_3">
-					<h3>Films in Theaters</h3>
-					
-					<?php
-          	 $today=date("Y-m-d");
-          	$qry2=mysqli_query($con,"select * from  tbl_movie where status='0' order by rand() limit 3");
-						
-          	  while($m=mysqli_fetch_array($qry2))
+			<div class="col-md-8 left-box">
+				<?php
+          	 	$today=date("Y-m-d");
+          		$qry2=mysqli_query($con,"select * from  tbl_movie where status='0' order by rand() limit 3");
+					while($m=mysqli_fetch_array($qry2))
                    {
                     ?>
-            <div class="content-left">
+            
 					<div class="listimg listimg_1_of_2">
 					<?php
 						
 						?>
-						 <a href="about.php?id=<?php echo $m['movie_id'];?>"><img src="<?php echo $m['image'];?>"></a>
+
 					</div>
-					<div class="text list_1_of_2">
-						  <div class="extra-wrap1">
-                                         <a href="about.php?id=<?php echo $m['movie_id'];?>" class="link4"><?php echo $m['movie_name'];?></a><br>
-                                        <span class="data">Release Date:<?php echo $m['release_date'];?></span><br>
-                                        Cast:<Span class="data"><?php echo $m['cast'];?></span><br>
-                                        Description: <span" class="color2"><?php echo $m['desc'];?></span><br>
-                                    </div>
-					</div>
-					
+					<a href="about.php?id=<?php echo $m['movie_id'];?>" class="link4"><h4><?php echo $m['movie_name'];?></h4></a>
+			                 <span class="data"><b>Release Date: </b><?php echo $m['release_date'];?></span><br>
+			                <b>Cast: </b><Span class="data"><?php echo $m['cast'];?></span><br>
+			                <b>Description: </b> <span" class="color2"><?php echo $m['desc'];?></span><br>
+		    </div>
+			<div class="col-md-8 text-right">
+				<img style="width:200px" class="movie-img" src="<?php echo $m['image']; ?>" alt=""/>
+			</div>
+			
 					<div class="clear"></div>
-				</div>
+		</div>
   	    <?php
   	    	}
-  	    	?>
-					
-					
-				
-				
-					
-					
-				
-				
-				
-				
+  	    	?>		
 				</div>		
 				<div class="clear"></div>	
+		
 	</div>
+	<?php include('footer.php');?>
 </div>
-<?php include('footer.php');?>
-</div>
-<?php include('searchbar.php');?>
