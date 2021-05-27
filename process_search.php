0265453e4224eded6005c6afabcb4c27f497135d
renata242 <59351081+renata242@users.noreply.github.com>
2021-05-27 11:32:58 +0600

    Add files via upload

---
+++ process_search.php
@@ -0,0 +1,54 @@
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cinema</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <style>
+    </style>
+</head>
+<?php include('header.php');
+extract($_POST);
+?>
+</div>
+<div class="content">
+
+	<div class="wrap">
+		<div class="content-top">
+			<h3>Movies</h3>
+			
+			<?php
+          	 $today=date("Y-m-d");
+          	$qry2=mysqli_query($con,"select DISTINCT movie_name,movie_id,image,cast from tbl_movie where movie_name='".$search."'");
+						
+          	  while($m=mysqli_fetch_array($qry2))
+                   {
+                    ?>
+                    
+                    <div class="col_1_of_4 span_1_of_4">
+					<div class="imageRow">
+						  	<div class="single">
+						
+						  		<a href="about.php?id=<?php echo $m['movie_id'];?>" rel="lightbox"><img  style="width:200px" class="movie-img" src="<?php echo $m['image'];?>" alt="" /></a>
+						  	</div>
+						  	<div class="movie-text">
+						  		<h4 class="h-text"><a href="about.php?id=<?php echo $m['movie_id'];?>"><?php echo $m['movie_name'];?></a></h4>
+						  		<b>Cast:</b><Span class="color2"><?php echo $m['cast'];?></span><br>
+			               
+						  		
+						  	</div>
+		  				</div>
+		  		</div>
+		  		
+  	    <?php
+  	    	}
+  	    	?>
+			
+			</div>
+				<div class="clear"></div>		
+			</div>
+			<?php include('footer.php');?>
