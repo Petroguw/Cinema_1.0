268e251f2d6ad3eeee1689b9784ace2667717c69
orazbaym <68743608+orazbaym@users.noreply.github.com>
2021-05-28 14:12:32 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,105 @@
+<style type="text/css">
+	.title{
+		font-style: normal;
+		font-weight: bold;
+		font-size: 20px;
+		line-height: 24px;
+		padding-top: 120px;
+		letter-spacing: 0.05em;
+		color: white;
+	}
+	a{
+		font-family: Montserrat;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 16px;
+		line-height: 24px;
+		letter-spacing: -0.015em;
+		color: rgba(255, 255, 255, 0.8);
+	}
+	a:link {
+  		text-decoration: none;
+	}
+	a:hover {
+		color: #d4afff;
+	}
+
+	button{
+		width: 130px;
+		height: 40px;
+		border: 1px solid #000000;
+		box-sizing: border-box;
+		background-color: #d4afff;
+		font-family: Montserrat;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 20px;
+		line-height: 24px;
+	}
+</style>
+
+<link rel="stylesheet" href="Main.css">
+<hr>
+	<footer class="align-bottom" style="background-color: #37003c;">
+		<div class="container-fluid">
+      		<div class="row">
+      			<div class="col-lg-1"></div>
+      			<div class="col-lg-2">
+      				<a href=""><img src="logo_footer.png" alt="phoneicon" class="navbar-brand" style="width: 240px; padding-top: 75px;"></a>
+      			</div>
+      			<div class="col-lg-2"></div>
+      			<div class="col-lg-2">
+      				<p class="title">RIM CINEMA</p>
+      				<a href="#">Terms & Conditions<br></a>
+      				<a href="#">Policies<br></a>
+					<a href="#">Cookie Policy</a>
+      			</div>
+      			<div class="col-lg-2">
+      				<p class="title">Community</p>
+      				<a href="#">Films<br></a>
+      				<a href="#">Media<br></a>
+      				<a href="#">Booking</a>
+					</b>
+      			</div>
+      			<div class="col-lg-1">
+      				<p class="title">Company</p>
+      					<a href="#">About us<br></a>
+      					<a href="#">Contact us<br></a>
+      					<a href="#">History</a>	
+					</b>
+      			</div>	
+      			<div class="col-lg-2">
+      				<p class="title" style="padding-top: 100px;"></p>
+      				<button type="button" class="btn btn-lg">Register</button><br><br>
+      				<button type="button" class="btn btn-light btn-lg">Login</button>
+      			</div>
+
+      			<div class="col-lg-1"></div>
+      			<div class="col-lg-10">
+      				<hr style="background-color: rgba(255, 255, 255, 0.5); margin-top: 60px;">
+      			</div>
+      			<div class="col-lg-1"></div>
+      			<div class="col-lg-1"></div>
+      			<div class="col-lg-2">
+      				<a style="color: rgba(255, 255, 255, 0.8);"> © RIM, Inc. 2021. We love our users!</a>
+      			</div>
+      			<div class="col-lg-6"></div>
+      			<div class="col-lg-3">
+      				<b> Follow us : </b>
+      				<a href=""><img src="logos_sm/telegram.png" alt="phoneicon" class="navbar-brand" style="width: 44px;"></a>
+      				<a href=""><img src="logos_sm/github.png" alt="phoneicon" class="navbar-brand" style="width: 44px;"></a>
+      				<a href=""><img src="logos_sm/instagram.png" alt="phoneicon" class="navbar-brand" style="width: 44px;"></a>
+      				<a href=""><img src="logos_sm/youtube.png" alt="phoneicon" class="navbar-brand" style="width: 44px;"></a>
+      			</div>	
+      		</div>
+      	</div>
+  	
+	</footer>
+	
+	
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
