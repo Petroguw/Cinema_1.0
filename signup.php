bbe75f35a364a2b0600f364a2f3b3a485ee7e745
renata242 <59351081+renata242@users.noreply.github.com>
2021-06-02 12:59:57 +0600

    Add files via upload

--- signup.php
+++ signup.php
@@ -1,13 +1,3 @@
-<?php
-  session_start();
-  if(isset($_SESSION['user'])){
-          header('Location: ../CodingLab/profile.php');
-  } 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,60 +12,63 @@
 
 <body>
   <?php
-    require 'header.php'
+  require 'header.php'
   ?>
-	
-  <div class="block">
+<link rel="stylesheet" href="validation/dist/css/bootstrapValidator.css"/>
+    
+<script type="text/javascript" src="validation/dist/js/bootstrapValidator.js"></script>
+  <!-- =============================================== -->
+  <?php
+    include('form.php');
+    $frm=new formBuilder;      
+  ?> 
+</div>
+<div class="block">
     <div class="wrapper">
-      <div class="title">Sign up</div>
-        <form action="signupCheck.php" method="post">
-          <div class="field">
-            <input type="text" name="full_name" required>
-            <label>Full name</label>
-          </div>
-          <div class="field">
-            <input type="text" name="login" required>
-            <label>Login</label>
-          </div>
-          <div class="field">
-            <input type="text" name="email" required>
-            <label>Email Address</label>
-          </div>
-          <div class="field">
-            <input type="password" name="password" required>
-            <label>Password</label>
-          </div>
-          <div class="field">
-            <input type="password" name="password_confirm" required>
-            <label>Repeat password</label>
-          </div>
-          <div class="content">
-          </div>
-          <div class="field">
-            <input type="submit" value="Sign up">
-          </div>
-          <div class="signup-link">
-            Already a member? <a href="login.php">Sign in</a>
-          </div>
-          
-            <?php 
-              if(isset($_SESSION['message'])){
-                echo '<p class="msg"> ' . $_SESSION['message'] .' </p>';
-              }
-              unset($_SESSION['message']);
-            ?>
-          
-        </form>
+        <div class="title">Sign up</div>
+          <div class="panel-body">
+        <form action="process_registration.php" method="post" id="form1">
+        <div class="field">
+        <input name="name" type="text" size="25" placeholder="Name" class="form-control"/>
+        <?php $frm->validate("name",array("required","label"=>"Name","regexp"=>"name"));  ?>
+      </div>
+      <div class="field">
+        <input name="age" type="text" size="25" placeholder="Age" class="form-control"/>
+        <?php $frm->validate("age",array("required","label"=>"Age","regexp"=>"age"));  ?>
+      </div>
+      <div class="field">
+        <select name="gender" class="form-control">
+            <option value >Select Gender</option>
+            <option>Male</option>
+            <option>Female</option>
+        </select>
+        <?php $frm->validate("gender",array("required","label"=>"Gender"));  ?>
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="field">
+        <input name="phone" type="text" size="25" placeholder="Mobile Number" class="form-control"/>
+        <?php $frm->validate("phone",array("required","label"=>"Mobile Number","regexp"=>"mobile"));  ?>
+      </div>
+      <div class="field">
+        <input name="email" type="text" size="25" placeholder="Email" class="form-control"/>
+        <?php $frm->validate("email",array("required","label"=>"Email","email")); ?>
+      </div>
+      <div class="field">
+        <input name="password" type="password" size="25" placeholder="Password" class="form-control" placeholder="Password" />
+        <?php $frm->validate("password",array("required","label"=>"Password","min"=>"7"));  ?>
+      </div>
+      <div class="field">
+        <input name="cpassword" type="password" size="25" placeholder="Password" class="form-control" placeholder="Password" />
+        <?php $frm->validate("cpassword",array("required","label"=>"Confirm Password","min"=>"7","identical"=>"password Password"));?>
+      </div>
+      <div class="field">
+          <button type="submit" class="btn btn-primary">Login</button>
+      </div>
+      </div>
+</div>
+    </form>
+      </div>
     </div>
-  </div>
-
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+<script>
+        <?php $frm->applyvalidations("form1");?>
+    </script>
