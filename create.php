1220b1d58c211e4e4b9b06e0ebc031b6659836d0
orazbaym <68743608+orazbaym@users.noreply.github.com>
2021-06-03 16:26:56 +0600

    Update create.php

--- create.php
+++ create.php
@@ -1,11 +1,15 @@
 <?php
 
 require_once 'db.php';
-
+$t_id = $_POST['t_id'];
 $name = $_POST['name'];
-$year = $_POST['goals'];
-$genre = $_POST['goals'];
-$rating = $_POST['goals'];
-mysqli_query($connect,"INSERT INTO `films` (`id`, `name`, `year`, `genre`, `rating`) VALUES (NULL, '$name', '$year', '$genre', '$rating')");
+$year = $_POST['year'];
+$genre = $_POST['genre'];
+$desc = $_POST['desc'];
+$rating = $_POST['rating'];
+$image = $_POST['image'];
+$video_url = $_POST['video_url'];
+
+mysqli_query($connect,"INSERT INTO `films`(`id`, `t_id`, `name`, `year`, `genre`, `desc`, `rating`, `image`, `video_url`) VALUES (NULL, '$t_id', '$name', '$year', '$genre', '$desc', '$rating', '$image' , '$video_url')");
 
 header('Location: /FINAL/adminpage.php');
